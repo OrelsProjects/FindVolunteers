b4451e4b76dfc7ae69c4ffef425e2d6b0aa71c43
no12345678 <mortz12345@gmail.com>
2023-10-20 14:34:29 +0300

    add shadcn components + add form fields to volunteer card in edit mode

--- app/new-volunteer/_components/volunteerCard.tsx
+++ app/new-volunteer/_components/volunteerCard.tsx
@@ -1,8 +1,37 @@
 "use client";
 
-import { Button } from "@mui/material";
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Pencil } from "lucide-react";
 import { useState } from "react";
-import EditIcon from "@mui/icons-material/Edit";
+import { useForm } from "react-hook-form";
+import * as z from "zod";
+
+const formSchema = z.object({
+  name: z
+    .string()
+    .trim()
+    .min(2, "יש להזין שם באורך של לפחות 2 תווים ועד 50")
+    .max(50),
+  role: z
+    .string()
+    .trim()
+    .min(2, "יש להזין שם באורך של לפחות 2 תווים ועד 50")
+    .max(50),
+  experienceYears: z.coerce
+    .number({ invalid_type_error: "יש להזין מספר תקין גדול מ-0" })
+    .positive("יש להזין מספר תקין גדול מ-0")
+    .max(99, "לא נסחפנו?"),
+});
 
 interface Props {
   name: string;
@@ -13,23 +42,40 @@ interface Props {
 const VolunteerCard = ({ name, role, experienceYears }: Props) => {
   const [editMode, setEditMode] = useState(false);
 
+  const form = useForm<z.infer<typeof formSchema>>({
+    // reValidateMode: 'onChange',
+    mode: "onBlur",
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      name: "",
+      role: "",
+      experienceYears: 1,
+    },
+  });
+
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    console.log(values);
+  }
+
   const onEditModeClick = () => {
     console.log("edit mode clicked");
-    setEditMode((prevValue) => !prevValue);
+    setEditMode(true);
+  };
+
+  const onCancel = () => {
+    form.reset();
+    setEditMode(false);
   };
 
   return (
     <div className="shadow-sm rounded-lg border border-[hsl(240_5.9%_90%)] w-[350px] p-6 flex flex-col gap-6">
       <div className="flex justify-between items-center">
         <span className="text-2xl font-semibold">כרטיס מתנדב</span>
-        <Button
-          variant="text"
-          color="inherit"
-          startIcon={<EditIcon className="ml-1" />}
-          onClick={onEditModeClick}
-        >
-          עריכה
-        </Button>
+        {!editMode && (
+          <Button variant="ghost" onClick={onEditModeClick}>
+            <Pencil className="ml-2 h-4 w-4" /> עריכה
+          </Button>
+        )}
       </div>
       {!editMode && (
         <>
@@ -52,10 +98,65 @@ const VolunteerCard = ({ name, role, experienceYears }: Props) => {
       )}
       {editMode && (
         <div>
-          <div className="flex justify-between">
-            <Button variant="outlined" color="inherit" className="px-4 py-2 text-sm hover:bg-[hsl(240_4.8%_95.9%)]">ביטול</Button>
-            <Button variant="contained" className="bg-[hsl(240_5.9%_10%)] hover:bg-[hsl(240_5.9%_10%/.9)] text-sm">אישור</Button>
-          </div>
+          <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
+              <FormField
+                control={form.control}
+                name="name"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel className="font-semibold text-base">
+                      שם:
+                    </FormLabel>
+                    <FormControl>
+                      {/* <Input placeholder="" {...field} /> */}
+                      <Input {...field} />
+                    </FormControl>
+                    {/* <FormDescription>
+                      This is your public display name.
+                    </FormDescription> */}
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="role"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel className="font-semibold text-base">
+                      תפקיד:
+                    </FormLabel>
+                    <FormControl>
+                      <Input {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="experienceYears"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel className="font-semibold text-base">
+                      שנות ניסיון:
+                    </FormLabel>
+                    <FormControl>
+                      <Input {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <div className="flex justify-between pt-2">
+                <Button variant="outline" onClick={onCancel}>
+                  ביטול
+                </Button>
+                <Button type="submit">אישור</Button>
+              </div>
+            </form>
+          </Form>
         </div>
       )}
     </div>
