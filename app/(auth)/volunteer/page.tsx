c9a3edba8fd81ce1d9b6e6c65c686201c9654eb9
no12345678 <mortz12345@gmail.com>
2023-10-22 07:13:23 +0300

    minor change

--- app/(auth)/volunteer/page.tsx
+++ app/(auth)/volunteer/page.tsx
@@ -6,16 +6,16 @@ import LinkedinLogin from "../../../components/LinkedinLogin";
 
 const Volunteer = () => {
   return (
-    <div className="flex flex-col items-center justify-center h-screen p-8">
+    <div className="flex flex-col items-center justify-center h-screen p-8 text-center">
       <h1 className="font-bold text-2xl">
         תודה רבה על רוח ההתנדבות!
         <br /> נעשה כל שביכולנתו על מנת לקשר אותך לאנשים הנכונים
       </h1>
-      <p>
-        למטרת שמירה על הביטחון שלכם\ן ושל כולנו, נבקש להתחבר באמצעות Linkedin
+      <p className="mt-4">
+        למטרת שמירה על הביטחון שלכם/ן ושל כולנו, נבקש להתחבר באמצעות Linkedin
         על מנת לאמת את זהותכם
       </p>
-      <p className="text-sm">
+      <p className="text-sm mb-4">
         *פרופיל הLinkedin ישמש גם כצורת ההתקשרות ביניכם/ן וממנו נמשוך את הפרטים
         על מנת לייצר כרטיס מתנדב
       </p>
