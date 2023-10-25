b8309c3d76b85c980026cccc5b173820a87fb41a
orelsprojects <orelsmail@gmail.com>
2023-10-25 13:50:16 +0300

    Fix requests

--- app/api/volunteer/route.ts
+++ app/api/volunteer/route.ts
@@ -1,14 +1,52 @@
 import { volunteersCol } from "@/utils/firestore";
-import { addDoc, deleteDoc, doc, getDoc, updateDoc } from "firebase/firestore";
+import {
+  addDoc,
+  deleteDoc,
+  doc,
+  getDoc,
+  orderBy,
+  query,
+  updateDoc,
+  limit,
+  getDocs,
+} from "firebase/firestore";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(req: NextRequest): Promise<NextResponse | void> {
   try {
     const id = req.nextUrl.searchParams.get("id") || "";
-    const volunteer = await getDoc(doc(volunteersCol, id));
+    // Many volunteers request
+    if (!id) {
+      const pageStr = req.nextUrl.searchParams.get("page");
+      const limitStr = req.nextUrl.searchParams.get("limit");
+      // Validate the query params
+      if (pageStr === null && limitStr === null) {
+        throw new Error("Invalid query");
+      }
 
-    return NextResponse.json(volunteer, { status: 200 });
+      const pageNum: number = pageStr !== null ? Number(pageStr) : 1;
+      const limitNum: number = limitStr !== null ? Number(limitStr) : 10;
+
+      if (isNaN(pageNum) || isNaN(limitNum)) {
+        throw new Error("Invalid query parameters");
+      }
+
+      const q = query(
+        volunteersCol,
+        orderBy("createdAt", "desc"),
+        limit(limitNum)
+      );
+      const snapshot = await getDocs(q);
+      const volunteers = snapshot.docs.map((doc) => doc.data());
+      return NextResponse.json(volunteers, { status: 200 });
+    } else {
+      // Single volunteer request
+      const volunteer = await getDoc(doc(volunteersCol, id));
+      const volunteerData = volunteer.data();
+      return NextResponse.json(volunteerData, { status: 200 });
+    }
   } catch (e) {
+    console.log(e);
     return NextResponse.json({ error: "Request failed" }, { status: 500 });
   }
 }
@@ -16,6 +54,7 @@ export async function GET(req: NextRequest): Promise<NextResponse | void> {
 export async function POST(req: NextRequest): Promise<NextResponse | void> {
   try {
     const volunteer = await req.json();
+    volunteer.createdAt = new Date();
     const volunteerRef = await addDoc(volunteersCol, volunteer);
 
     return NextResponse.json({ id: volunteerRef.id }, { status: 200 });
@@ -27,20 +66,21 @@ export async function POST(req: NextRequest): Promise<NextResponse | void> {
 export async function PUT(req: NextRequest): Promise<NextResponse | void> {
   try {
     const volunteer = await req.json();
+    volunteer.lastUpdatedAt = new Date();
     const volunteerRef = doc(volunteersCol, volunteer.id);
 
-    updateDoc(volunteerRef, volunteer.toDocument());
+    updateDoc(volunteerRef, volunteer);
 
-    return NextResponse.json({ id: volunteerRef.id }, { status: 200 });
+    return NextResponse.json({}, { status: 200 });
   } catch (e) {
-    return NextResponse.json({ error: JSON.stringify(e) }, { status: 500 });
+    console.log(e);
+    return NextResponse.json({ error: "Request failed" }, { status: 500 });
   }
 }
 
 export async function DELETE(req: NextRequest): Promise<NextResponse | void> {
   try {
     const volunteer = await req.json();
-
     deleteDoc(doc(volunteersCol, volunteer.id));
 
     return NextResponse.json({}, { status: 200 });
